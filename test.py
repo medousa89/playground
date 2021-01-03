e9000143dd8f666de17b67b2f929e4fff89f305a
Athanasios Oikonomou <athoik@gmail.com>
2021-01-04 01:11:08 +0200

    Create test.py

---
+++ test.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# -*- encoding: utf-8 -*-
+
+from __future__ import print_function
+import datetime
+from xml.sax.saxutils import escape
+from bs4 import BeautifulSoup
+import requests
